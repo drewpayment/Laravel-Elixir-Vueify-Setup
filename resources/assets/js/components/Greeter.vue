0573dfbd9c545a156d28ef39e573161423050300
= <=>
2016-03-21 23:40:59 -0400

    - Changed view end point to public/views in order to take first step to creating single page app from Vue.js integrated with Laravel API

--- resources/assets/js/components/Greeter.vue
+++ resources/assets/js/components/Greeter.vue
@@ -2,6 +2,8 @@
     <div>
         <h1>Hello, {{ name }}!</h1>
 
+        <p>The above snippet is a great example of how Vue.js works to make our life easier.</p>
+
         <input type="text" v-model="name">
     </div>
 </template>
@@ -10,7 +12,7 @@
     export default {
         data() {
             return {
-                name: 'Laracasts'
+                name: 'new developer'
             };
         }
     };
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -3,11 +3,9 @@ var Vue = require('vue');
 import Greeter from './components/Greeter.vue';
 
 new Vue({
+
     el: '#app',
 
-    components: { Greeter },
+    components: { Greeter }
 
-    ready() {
-        alert('Vue and Vueify all set to go!');
-    }
 });
